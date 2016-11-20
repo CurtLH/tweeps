26389dbf027db649dc5ed96bd58bf09cac30df1a
CurtLH <CurtLHampton@gmail.com>
2016-11-19 21:44:40 -0600

    stop tracking changes

--- oauth_creds.py
+++ oauth_creds.py
@@ -1,4 +1,4 @@
-consumer_key = 'xxxxxxxxxxxxxxxxxxxxxx'
-consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-access_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+consumer_key = 'qfpeU1pBcz1dbTc0X80sVQ'
+consumer_secret = 'Y62hVldCPKq6kJXu3S88Oc2HEpc2Ad45hejW5vwz10'
+access_token = '143643166-PMBzANRyh0ti5gSaAQASvmqg6jyRhGQZz9IwviO2'
+access_secret = 'B4ECLEYO0wqYqQKeYQLaSTjx5SCvsg7oClH9keM'
