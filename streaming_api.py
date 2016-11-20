3139b267ca502e51f0473dbb13e0449690465afb
CurtLH <CurtLHampton@gmail.com>
2016-11-19 21:57:01 -0600

    fixed pep8 issues

--- streaming_api.py
+++ streaming_api.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 
 import logging
-import time
-import csv
 import json
 import oauth_creds as oauth
 import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
-from datetime import datetime
-from dateutil import parser
 
 
 # enable logging
-logging.basicConfig(level=logging.INFO, 
-                    format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s', 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
                     datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -29,40 +25,40 @@ api = tweepy.API(auth)
 
 # establish open connection to streaming API
 class MyListener(StreamListener):
-    
+
     def on_data(self, data):
         try:
             tweet = json.loads(data)
-            with open('tweets.json', 'a') as f: 
+            with open('tweets.json', 'a') as f:
                 json.dump(tweet, f)
                 f.write('\n')
 
         except BaseException as e:
             logger.warning(e)
-        
+
         return True
-        
+
     def on_error(self, status):
         logger.warning(status)
         return True
 
 
 def start_stream():
-   
+
     terms = list([line.strip() for line in open('terms.txt')])
-    
+
     while True:
-        
+
         logger.warning("Twitter API Connection opened")
-        
+
         try:
             twitter_stream = Stream(auth, MyListener())
             twitter_stream.filter(track=terms)
-        
-        except Exception as e: 
+
+        except Exception as e:
             logger.warning(e)
             continue
-        
+
         finally:
             logger.warning("Twitter API Connection closed")
 
