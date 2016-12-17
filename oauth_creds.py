82166c631a8b7f116ddf56df9a71aa5c41436c6b
CurtLH <CurtLHampton@gmail.com>
2016-12-17 10:18:28 -0600

    removed for privacy

--- oauth_creds.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'qfpeU1pBcz1dbTc0X80sVQ'
-consumer_secret = 'Y62hVldCPKq6kJXu3S88Oc2HEpc2Ad45hejW5vwz10'
-access_token = '143643166-PMBzANRyh0ti5gSaAQASvmqg6jyRhGQZz9IwviO2'
-access_secret = 'B4ECLEYO0wqYqQKeYQLaSTjx5SCvsg7oClH9keM'
