901adf4ba774024613f48ea6876d3ddc62422ffe
Curtis H <CurtLHampton@gmail.com>
2017-10-31 19:52:10 -0400

    Transition into conda package

--- tweeps/cli.py
+++ tweeps/cli.py
@@ -1,2 +1,15 @@
+import click
+from tweeps import streaming_api
+from tweeps import etl_process
+
+@click.group()
 def cli():
-    return "CLI template"
+
+    """
+    Twitter Streaming API and ETL Process
+    """
+
+    pass
+
+cli.add.commands(streaming_api.cli, 'api')
+cli.add_commands(etl_process.cli, 'etl') 
