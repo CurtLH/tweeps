8e493c0affcde9ba0b54dadeb87519d370e25bd2
Curtis H <CurtLHampton@gmail.com>
2017-10-31 19:37:20 -0400

    Add components of conda package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [
 setup(
     name='tweeps',
     version='0.1.0',
-    description="The tweeps application provides a simple way to connect to the the Twitter Streaming API. After you submit your keywords and hashtags to tweeps, the application will maintain a constant connection to the API, and store new tweets in a PostgreSQL database as they come into the system. The application also includes an ETL process that will regularily monitor the source database and routinely extract, transform, and load data from the source database into a data warehouse.",
+    description="Short description",
     author="Curtis Hampton",
     author_email='CurtLHampton@gmail.com',
     url='https://github.com/CurtLH/tweeps',
